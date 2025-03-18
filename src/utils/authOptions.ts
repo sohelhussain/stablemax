3d0ba8d2b961b3a1ebb92cec352365e10fde1858
sohelhussain <sohelhussaing@gmail.com>
2025-03-18 21:22:45 +0530

    fix: ADDING NEXT_SECRET

--- src/utils/authOptions.ts
+++ src/utils/authOptions.ts
@@ -11,6 +11,7 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async session({ session }) {
       const user = await prisma.user.findUnique({
